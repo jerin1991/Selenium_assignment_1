c962d2c607bb8de5b3362011ac1f68d695cf7b7a
jerry1991 <jerin.johny22@gmail.com>
2022-03-20 16:58:09 +0530

    assignment

--- comparing_price.py
+++ comparing_price.py
@@ -4,5 +4,5 @@ from assignment1 import open_amazon
 time.sleep(5)
 from assignment1 import open_flipkart
 time.sleep(5)
-price_az = open_amazon.amazon_price()
-price_fk = open_flipkart.flipkart_price()
+price_az = open_amazon.amazon_price() #calling amazon_price from open_amazon class
+price_fk = open_flipkart.flipkart_price() #calling flipkart_price from open_flipkart class
